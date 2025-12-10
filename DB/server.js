a7f065de342a846a22b3df7b7112f9ac585ad98a
Prateek <rahuls12153@gmail.com>
2025-12-10 13:20:52 +0530

    authentication changes

--- DB/server.js
+++ DB/server.js
@@ -40,6 +40,11 @@ app.use(cors({
   credentials: true
 }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 
 
 app.listen(PORT , async() => {
